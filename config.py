d04c5b2f49caf356a8e686522e6d3d9258571fbe
yutaokkate-oss <yutaokkate@gmail.com>
2026-03-26 10:38:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("32426469"))
+API_HASH = getenv("6cc009055eb3c64d5b50d6cea35ebee8")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7981788706:AAEpCPckAoFdDRokiNP5dqB69N-YwmvxVXQ")
 
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://s72222980_db_user:43UUH5CnqQ19cnTW@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ABOUTxYUTA")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MOONIEEZ")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+NQGCtLEJJEo3OWY1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -63,8 +63,8 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = getenv("BQJdu6kAoX7BWrAj4fppBsqLWTkYriNmpu7eEl7SVaRG6K_b9YLPwI3RLTqmOL7zdrfJYbyy1vf3nRl_MZ5alwKQ12eKIN26v8bdFtR_iZGhwLLoSNSCwI0ny-wWge2FEc3DOEJ1kGgux0EwqdSQ6GnYeydPuIJ0cPRvMxpscGPnDcvFLbCZRE34VZlTAMk3F3NLHzvg2MqtlQ5dGIfZYMceIwfxtZF8cWj3iJO99RroAXsqtVKURwi9LUHiWX2iRMPKU-psn5Q5Lks3Nkc9I-7grWgP0u2UNZa6OTFjJ7wCATYqLN3944Ewjk_qptVt6RGdTUVRZ8avhWJYOzsn6poDt7pTEAAAAAGZ_ZuoAA", None)
-STRING2 = getenv("BAJdu6kAmK6JNAoTzvvcY_U0j7kO6n7ZfZ0fnXz-Kh7Tq6bikUyu5Y06NTPMOwp_VtuWJYvaS4uNmFiDiKa38PCKFi9NpS3I15EYUucFYydXl-AoW_V_jEYGTYzpwrRXkh7plbfq9U-5wECeg30fI_y7MKHVV8bMBHdrCKKNS8mJAo1haMDef2V9KO6cxUDYFDHOSgoxNPFh_mBO5lBjPps-v6nb1gCbBZsi8WVDZYitoaBKQ4HMVWBiJzPXucn14lHxw-0576fPmYD9Brtz_6-U8_gru26wJkC_1g0i6n9J1dGPLcOMxVHrMC3rqHI0ImnmYd4URrIWfPkM1CZLH96Vzrl0wAAAAAIEB3suAA", None)
+STRING1 = getenv("BAJdu6kAWWID1CsbRuIZDOckUNwWWqncTKbTf-8SDL2koa70qgrdBHdVhFkJag4yem2UKCAEQZaHJSzBGx6Wvsq9WDNdgRHOa-nlameaeJdyS_o5iIAcgOw-GS7E-sKGSnmCPW5dkOBVS0SBpzbAHuhFWwKiAJpeelb0ATsCwzrqJ_n7VYOyy_TC_JDsgCAos_I2XWGZsnW8-67ZO1MNx5cspd7j_ME-GDrcvpcP2z0pIeTe1XQToTFkeTj71Ne5livMKZFlu-yXC4hUenPtyny-a9EoeTi8_C4sipwfm35qrjxhs6bSzEUGIF7ZIPFoyw7NYy10UgsYRvAviX-wrtrnY2fORQAAAAIFzOn8AA", None)
+STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
@@ -82,7 +82,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/66dl4g.mp4"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/66dl4g.mp4"
+    "PING_IMG_URL", "https://files.catbox.moe/w9o758.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/2d2v8w.mp4"
 STATS_IMG_URL = "https://files.catbox.moe/t229wh.jpg"
